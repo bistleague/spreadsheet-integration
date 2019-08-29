44ea6baae8e0281b7020fd1ee97f6c245e30f6b7
alfinm01 <arrestral@gmail.com>
2019-08-29 12:18:50 +0700

    Fix await problem

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ BIST League back-end to Google Spreadsheet integration
 
 ### Environment Variables
 ```dotenv
-PORT=<port>
+GOOGLE_SPREADSHEET_ID=<insert Google Spreadsheet ID>
 GCP_PROJECT_ID=<insert GCP project ID>
 GCP_DATASTORE_CREDENTIALS_JSON_PATH=<insert service account JSON path>
 ```
--- main.js
+++ main.js
@@ -10,69 +10,106 @@ const GoogleSpreadsheet = require('google-spreadsheet');
 const { promisify } = require('util');
 
 const creds = require(process.env.GCP_DATASTORE_CREDENTIALS_JSON_PATH);
-// const creds = require('./client_secret.json');
 
 const teamsRepository = require('./datastore/teams');
 const usersRepository = require('./datastore/users');
+const filesRepository = require('./datastore/files');
 
 /**
  * Convert document and stage status to string
  * {{ STILL INCOMPLETE }}
  */
-async function changeStatus(data) {
+async function changeStatus(data, sheetType) {
 	// TEAMS
-	// Proof of payment
-	if (data.proof_of_payment_verified) {
-		data.proof_of_payment_verified = "Verified";
-	} else {
-		data.proof_of_payment_verified = "Rejected";
-		if (data.proof_of_payment_id != null) {
-			data.proof_of_payment_verified = "Pending";
+	if (sheetType == "Teams") {
+		// Proof of payment
+		if (data.proof_of_payment_verified) {
+			data.proof_of_payment_verified = "Verified";
 		} else {
-			data.proof_of_payment_verified = "Not uploaded";
+			data.proof_of_payment_verified = "Rejected";
+			if (data.proof_of_payment_id != null) {
+				data.proof_of_payment_verified = "Pending";
+			} else {
+				data.proof_of_payment_verified = "Not uploaded";
+			}
+		}
+		// Team stage
+		if (data.stage == 0) { data.stage = "Registered"; }
+			else if (data.stage == 1) { data.stage = "Completed"; }
+			else if (data.stage == 2) { data.stage = "Submitted preliminary"; }
+			else if (data.stage == 3) { data.stage = "Qualified semifinal"; }
+			else if (data.stage == 4) { data.stage = "Submitted semifinal"; }
+			else if (data.stage == 5) { data.stage = "Qualified final"; }
+		// Files
+		if (typeof data.proof_of_payment_file === "undefined") {
+			data.proof_of_payment_file = {};
+			data.proof_of_payment_file.url = null;
+		}
+		if (typeof data.preliminary_submission_file === "undefined") {
+			data.preliminary_submission_file = {};
+			data.preliminary_submission_file.url = null;
+		}
+		if (typeof data.semifinal_submission_file === "undefined") {
+			data.semifinal_submission_file = {};
+			data.semifinal_submission_file.url = null;
+		}
+		if (data.preliminary_submission_last_submitted) {
+			data.preliminary_submission_last_submitted = new Date(data.preliminary_submission_last_submitted).toLocaleString();
+		}
+		if (data.semifinal_submission_last_submitted) {
+			data.semifinal_submission_last_submitted = new Date(data.semifinal_submission_last_submitted).toLocaleString();
 		}
 	}
-	// Team stage
-	if (data.stage == 0) { data.stage = "Registered"; }
-		else if (data.stage == 1) { data.stage = "Completed"; }
-		else if (data.stage == 2) { data.stage = "Submitted preliminary"; }
-		else if (data.stage == 3) { data.stage = "Qualified semifinal"; }
-		else if (data.stage == 4) { data.stage = "Submitted semifinal"; }
-		else if (data.stage == 5) { data.stage = "Qualified final"; }
 
 	// TEAM MEMBERS
-	// Gender
-	if (data.gender == 0) { data.gender = "Not mentioned" }
-		else if (data.gender == 1) { data.gender = "Male" }
-		else if (data.gender == 2) { data.gender = "Female" }
+	if (sheetType == "Team members") {
+		// Gender
+		if (data.gender == 0) { data.gender = "Not mentioned" }
+			else if (data.gender == 1) { data.gender = "Male" }
+			else if (data.gender == 2) { data.gender = "Female" }
+		// Files
+		if (typeof data.student_id_file === "undefined") {
+			data.student_id_file = {};
+			data.student_id_file.url = null;
+		}
+		if (typeof data.poe_file === "undefined") {
+			data.poe_file = {};
+			data.poe_file.url = null;
+		}
+	}
 }
 
 /**
  * Update all rows
+ * {{ INCONSISTENT }}
  */
 async function updateSheet(data, sheet, sheetRows, sheetType) {
-	// Delete existing rows
-	sheetRows.forEach(row => {
-		row.del();
-	});
-
 	// Add new rows
 	if (sheetType == "Teams") {
+		console.log('Add Teams rows');
 		for (var row of data) {
-			await changeStatus(row);
+			await changeStatus(row, sheetType);
 			const values = {
 				teamid: row.team_id,
 				teamname: row.name,
 				university: row.university,
+				proofofpaymenturl: row.proof_of_payment_file.url,
 				proofofpaymentstatus: row.proof_of_payment_verified,
-				stage: row.stage
+				stage: row.stage,
+				preliminarycaseurl: row.preliminary_submission_file.url,
+				preliminarycasesubmittime: row.preliminary_submission_last_submitted,
+				semifinalqualified: row.semifinal_qualified,
+				semifinalcaseurl: row.semifinal_submission_file.url,
+				semifinalcasesubmittime: row.semifinal_submission_last_submitted,
+				finalqualified: row.final_qualified
 			}
 			await promisify(sheet.addRow)(values);
 		}
 	}
 	if (sheetType == "Team members") {
+		console.log('Add Team members rows');
 		for (var row of data) {
-			await changeStatus(row);
+			await changeStatus(row, sheetType);
 			const values = {
 				userid: row.id,
 				name: row.name,
@@ -82,13 +119,14 @@ async function updateSheet(data, sheet, sheetRows, sheetType) {
 				major: row.major,
 				gender: row.gender,
 				phonenumber: row.mobile_no,
+				studentidurl: row.student_id_file.url,
 				studentidstatus: row.student_id_status,
-				poestatus: row.poe_status,
+				poeurl: row.poe_file.url,
+				poestatus: row.poe_status
 			}
 			await promisify(sheet.addRow)(values);
 		}
 	}
-
 	console.log(`${sheetType} sheet updated!`);
 }
 
@@ -97,21 +135,43 @@ async function updateSheet(data, sheet, sheetRows, sheetType) {
  * Accessing Google Spreadsheet
  */
 async function accessSpreadsheet() {
-	const doc = new GoogleSpreadsheet('1Cec0FCoiqPTXWYeMylkRQmjgAR_-U5aUcHbR70RkDvs');
+	const doc = new GoogleSpreadsheet(process.env.GOOGLE_SPREADSHEET_ID);
 	await promisify(doc.useServiceAccountAuth)(creds);
 	const info = await promisify(doc.getInfo)();
-	const teamsSheet = info.worksheets[0];
-	const teamMembersSheet = info.worksheets[1];
+	const timestamp = new Date().toLocaleString();
+
+	// Create new worksheets
+	doc.addWorksheet({
+		title: `Teams (${timestamp})`,
+		headers: [
+			'Team ID', 'Team name', 'University', 'Proof of payment URL', 'Proof of payment status',
+			'Stage', 'Preliminary case URL', 'Preliminary case submit time', 'Preliminary case status',
+			'Semifinal qualified', 'Semifinal case file URL', 'Semifinal case submit time',
+			'Semifinal case status', 'Final qualified'
+		]
+	}, () => {
+		doc.addWorksheet({
+			title: `Team Members (${timestamp})`,
+			headers: [
+				'User ID', 'Name', 'Email', 'Team ID', 'University', 'Major', 'Student ID URL',
+				'Student ID status', 'PoE URL', 'PoE status', 'Gender', 'Phone number'
+			]
+		});
+	});
 
 	// Update teams
-	let teams = await teamsRepository.getAllTeams();
-	let teamRows = await promisify(teamsSheet.getRows)();
-	updateSheet(teams, teamsSheet, teamRows, "Teams");
+	let team = await teamsRepository.getAllTeams();
+	let teams = await filesRepository.getAllFiles(team);
+	updateSheet(teams, info.worksheets[2], null, "Teams");
 
 	// Update users
-	let users = await usersRepository.getAllUsers();
-	let userRows = await promisify(teamMembersSheet.getRows)();
-	updateSheet(users, teamMembersSheet, userRows, "Team members");
+	let user = await usersRepository.getAllUsers();
+	let users = await filesRepository.getAllFiles(user);
+	updateSheet(users, info.worksheets[3], null, "Team members");
+	
+	// Delete old sheets
+	await promisify(info.worksheets[0].del)();
+	await promisify(info.worksheets[1].del)();
 }
 
 accessSpreadsheet();
